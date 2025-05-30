cc4fb356b0b81ae14c7947b3746231f74d4385ee
Abbad01 <140261539+Abbad01@users.noreply.github.com>
2025-05-30 02:44:00 +0000

    varibales discussed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+
+const SchoolId=48;// can't be changed
+let Email="mammama@gmail.com"
+var password="20202"
+city="jaipur"
+let state;// undefined
+//SchoolId=56;// throw error
+// console.log(SchoolId);
+
+//changing values in other variables that are not constant.
+Email="bfikwbefi@gmail.com"
+password="84924"
+city="gayaji"
+console.table([Email,password,city]);
+//all values are changed,
+
+//var and let are used to declare varibales, and const for
+//making it constant(fixed).
+
+/* Prefer not to use var because of issue in block scope 
+and functional scope.*/
+
+/*If values are not assigned to variables they are undefined */

c32d2a7c0b477f726a7a0ad82f7a4a0430599aa9
Abbad01 <140261539+Abbad01@users.noreply.github.com>
2025-05-30 02:44:56 +0000

    datatypes discussed

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict" //treat all js code in new standards/versions
+//alert(3+3)// we re using node js, not browser
+//ECMAScript, js documentation.
+
+
+//primitve datatypes
+/* number:2^53
+bigint
+string:"gvgchh"
+boolean:true/false
+null:standalone value, khaali hai, it is an object
+undefined:value is not assigned
+symbol:unique */
+
+//non primitive datatypes
+//object
